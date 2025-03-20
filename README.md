2ec957f69d079af8c6027114a939617381db3995
Julien Vanharen <julien.vanharen@inria.fr>
2025-03-20 18:55:59 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# orbstack
-Ubuntu Orbstack configuration
+# Ubuntu Orbstack configuration
+
+Run the following command to create and set the virtual machine. Warning: this may take a while.
+
+```sh
+orb create ubuntu:noble ouranos && orb -m ouranos ./setup.sh
+```
+
+Run the follwing command to enter the Linux machine.
+
+```sh
+orb zsh
+```
---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Installing standard packages.
+sudo apt-get install --yes neofetch zsh git wget curl jq guix
+sudo apt-get update --yes
+
+# Installing dotfiles.
+curl --silent https://api.github.com/meta | jq --raw-output '"github.com "+.ssh_keys[]' >> $HOME/.ssh/known_hosts
+cd $HOME && yes | sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply --ssh jvanhare
+$HOME/bin/chezmoi update --force
+
+# Updating guix.
+guix pull
+guix install glibc-locales
+guix package -u
